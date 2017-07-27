66cb1dba5ac6a74f58a8ff5f589a50e0d8a1a178
穆坤 <mumukun@outlook.com>
2017-07-27 15:30:21 +0800

    Client app  init

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import './app.css';
+import React from 'react'
+import { browserHistory, Router } from 'react-router'
+import { Provider } from 'react-redux'
+import PropTypes from 'prop-types'
 
-class AppComponent extends React.Component {
+class App extends React.Component {
+  static propTypes = {
+    store: PropTypes.object.isRequired,
+    routes: PropTypes.object.isRequired,
+  }
 
-  render() {
+  shouldComponentUpdate () {
+    return false
+  }
+
+  render () {
     return (
-      <div className="index">
-        <div className="notice">
-          Please edit <code>src/components/App.js</code> to get started!
+      <Provider store={this.props.store}>
+        <div style={{ height: '100%' }}>
+          <Router history={browserHistory} children={this.props.routes} />
         </div>
-      </div>
-    );
+      </Provider>
+    )
   }
 }
 
-AppComponent.defaultProps = {
-};
-
-export default AppComponent;
+export default App
